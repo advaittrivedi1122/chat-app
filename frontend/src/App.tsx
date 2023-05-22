f3d66ba2037240c39cf3177c4b89d6cc05bb1f94
Advait Trivedi <trivediadvait0@gmail.com>
2023-05-23 00:36:40 +0530

    WebSocket connection works frontend backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,37 @@
 // import.meta.env.VITE_CHAT_URL
 import { useState, useEffect } from 'react'
 import './App.css'
+import useWebSocket from 'react-use-websocket'
 
 function App() {
   const [id, setID] = useState(0)
   const [message, setMessage] =useState("")
   const [messages, setMessages]: any = useState([])
   const url: any = import.meta.env.VITE_CHAT_URL
-  const ws = new WebSocket(url)
-
-  const fetchMessages = async ()=>{
-    const data = await fetch(url)
-    const parsedData = await data.json()
-    setMessages(parsedData)
-    return
-  }
+  // const ws = new WebSocket(url)
+  const { sendMessage: sendNewMessage } = useWebSocket(url, {
+    // share: true,
+    onMessage: (e)=> {
+      const parsedData: any = JSON.parse(e.data)
+      setMessages(parsedData)
+      console.log(`parsed`,parsedData)
+    }
+  })
 
   const sendMessage = async ()=>{
     console.log(message)
     const data = {id, message}
     console.log(messages)
-    ws.send(JSON.stringify(data))
+    sendNewMessage(JSON.stringify(data))
     setMessages([...messages, message])
     console.log(messages)
     return
   } 
+
   useEffect(()=>{
-    ws.onmessage = (e) => {
-      const parsedData: any = JSON.parse(e.data)
-      setMessages([...messages, parsedData?.message])
-      console.log(parsedData)
-    }
-    fetchMessages()
-  }, [messages])
+    console.log("re-rendered")
+    setMessages([...messages, message])
+  },[message])
 
   return (
     <>
@@ -41,8 +40,8 @@ function App() {
         <div className="messages">
           {
             messages.map((messageItem:any)=>{
-              <div className="message">
-                <h3>{messageItem}</h3>
+              return <div className="message">
+                <h3>{messageItem.message}</h3>
               </div>
             })
           }
